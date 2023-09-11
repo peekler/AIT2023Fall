590aa760a49faad9b5b9ae95a4b912de02bb6c1e
Péter Ekler <peter.ekler@aut.bme.hu>
2023-09-11 12:57:53 +0200

    Class3 demos
    SnackBar, Toast...
    Activity Life Cycle

--- AITTimeShower/app/src/main/java/hu/ait/aittimeshower/ui/MainActivity.kt
+++ AITTimeShower/app/src/main/java/hu/ait/aittimeshower/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package hu.ait.aittimeshower.ui
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.google.android.material.snackbar.Snackbar
 import hu.ait.aittimeshower.R
 import hu.ait.aittimeshower.databinding.ActivityMainBinding
 import java.util.Date
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    val UI_LOG_TAG = "TAG_UI"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,19 +28,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnTime.setOnClickListener {
-            //binding.tvTime.text = "${Date(System.currentTimeMillis()).toString()}"
-            //revealCard()
+            Log.d(UI_LOG_TAG,"btnTime was pressed")
 
-            var numA = binding.etNumA.text.toString().toInt()
-            var numB = binding.etNumB.text.toString().toInt()
-            binding.tvTime.text = "${numA+numB}"
 
+
+            //Toast.makeText(this,
+            //    "Hello ${binding.etUser.text.toString()}", Toast.LENGTH_LONG).show()
+
+            Snackbar.make(binding.root, "Hello ${binding.etUser.text.toString()}",
+                Snackbar.LENGTH_LONG).setAction("Undo", {
+                    // do something when undo pressed
+                }).show()
+
+            binding.tvTime.text =
+                getString(R.string.hello_the_time_is, Date(System.currentTimeMillis()).toString())
+            revealCard()
         }
     }
 
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun revealCard() {
+        Log.d(UI_LOG_TAG,"animation start")
+
         val x = binding.cardView.getRight()
         val y = binding.cardView.getBottom()
 
